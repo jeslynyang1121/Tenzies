bb98e00b8657b5b5a3dc2862b3b5129e820626ad
jeslynyang1121 <84688606+jeslynyang1121@users.noreply.github.com>
2025-07-23 21:53:29 -0700

    Add files via upload

---
+++ src/Die.jsx
@@ -0,0 +1,9 @@
+export default function Die(props) {
+    return (
+        <button 
+            className={props.isHeld ? "held" : ""}
+            onClick={props.hold}>
+            {props.value}
+        </button>
+    )
+}
